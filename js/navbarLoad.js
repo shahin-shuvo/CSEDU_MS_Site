d76d29fc2d23006bfcb4874416e0b4ab55f2c316
shahin-shuvo <shahinshuvo96@gmail.com>
2020-02-15 20:47:53 +0600

    Initial commit

---
+++ js/navbarLoad.js
@@ -0,0 +1,43 @@
+document.write('\
+\
+<div class="container-fluid" id="title-container">\
+  <img class="top-logo" src="images/du.png" alt="du logo">\
+  <div class="d-flex flex-column justify-content-around">\
+    <h2 class="du-name">University of Dhaka</h2>\
+    <h1 class="cse-name">Department of Computer Science & Engineering</h1>\
+  </div>\
+</div>\
+<nav class="navbar navbar-expand-lg py-md-0">\
+  <a class="navbar-brand" href="#">CSE DU</a>\
+  <button class="navbar-toggler ml-auto custom-toggler" type="button" data-toggle="collapse" data-target="#nav1" aria-controls="nav1" aria-expanded="false" aria-label="Toggle navigation">\
+    <span class="navbar-toggler-icon"></span>\
+  </button>\
+  <div class="collapse navbar-collapse" id="nav1" >\
+    <ul class="navbar-nav ml-auto">\
+      <li class="nav-item" id="nav-home">\
+        <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>\
+      </li>\
+      <li class="nav-item" id="nav-apply">\
+        <a class="nav-link" href="apply.html">Apply</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#">Notice</a>\
+      </li>\
+    <li class="nav-item dropdown">\
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
+        Dropdown link\
+      </a>\
+      <div class=" navbar-dropdown dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">\
+        <a class="dropdown-item" href="#">Action</a>\
+        <a class="dropdown-item" href="#">Another action</a>\
+        <a class="dropdown-item" href="#">Something else here</a>\
+      </div>\
+    </li>\
+    <li class="nav-item">\
+      <a class="nav-link" href="login.html">Login</a>\
+    </li>\
+  </ul>\
+  </div>\
+</nav>\
+\
+');
